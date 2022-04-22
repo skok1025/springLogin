8027b3eb76143c6e749b37b85653d3ae514eb716
김석현 <shkim30>
2022-04-22 16:33:51 +0900

    회원가입 기능

---
+++ src/main/java/hello/login/TestDataInit.java
@@ -0,0 +1,36 @@
+package hello.login;
+
+import hello.login.domain.item.Item;
+import hello.login.domain.item.ItemRepository;
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+
+@Component
+@RequiredArgsConstructor
+public class TestDataInit {
+
+    private final ItemRepository itemRepository;
+    private final MemberRepository memberRepository;
+
+    /**
+     * �뀒�뒪�듃�슜 �뜲�씠�꽣 異붽�
+     */
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 20));
+        
+        Member member = new Member();
+        member.setLoginId("test");
+        member.setPassword("test!");
+        
+        memberRepository.save(member);
+    }
+
+}
